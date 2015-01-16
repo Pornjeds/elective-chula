7d25a2bc92e0808555b1796e6c69375fc0451c01
gie3d <gie68i@gmail.com>
2015-01-16 08:13:54 +0700

    return json

--- server/admin_api.php
+++ server/admin_api.php
@@ -26,6 +26,7 @@ $app->configureMode('development', function () use ($app) {
     ));
 });
 
+//Rounting
 $app->group('/api/v1', function() use ($app){
 	$app->group('/student', function() use ($app){
 		$app->get('/test/:name', 'test');
@@ -42,19 +43,22 @@ function test($name)
 }
 
 function getStudentById($id){
+    $app = \Slim\Slim::getInstance();
 	$sql = "SELECT * FROM STUDENT where student_id = '$id'";
 	try {
 		$db = new DBManager();
 		$result = $db->getData($sql);
 		$db = null;
-		print_r($result);
+		$app->response->headers->set('Content-Type', 'application/json');
+        $app->response->setBody(json_encode($result));
 	} catch(PDOException $e) {
         echo '{"error":{"text":'. $e->getMessage() .'}}';
     }
 }
 
 function addStudent(){
-	$request = \Slim\Slim::getInstance()->request();
+    $app = \Slim\Slim::getInstance();
+	$request = $app->request();
     $student_detail = json_decode($request->getBody());
     $sql = "INSERT INTO STUDENT (student_id, classof_id, name, lastname, email, password, profilepic, GPA, addeddate, student_status) VALUES ('$student_detail->student_id', '$student_detail->classof_id', '$student_detail->name', '$student_detail->lastname', '$student_detail->email', '$student_detail->password', '$student_detail->profilepic', '$student_detail->GPA', GETDATE(), '$student_detail->student_status')";
     try {
@@ -62,11 +66,13 @@ function addStudent(){
         $db->setData($sql);
         if($db->beginSet())
         {
-        	$db->commitWork();	
+        	$db->commitWork();
+            $app->response->setBody(json_encode(array("status"=>"success")));
         }
         else
 		{
-			$db->rollbackWork();			
+			$db->rollbackWork();
+            $app->response->setBody(json_encode(array("status"=>"error")));		
 		}
         $db = null;
     } catch(PDOException $e) {
