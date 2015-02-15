b5d8716390cc7beb2f7ae0ce2b3bf6aa348b98c1
gie3d <gie68i@gmail.com>
2015-02-15 17:10:13 +0700

    check data duplication before insert/update classof

--- server/admin_api_classof.php
+++ server/admin_api_classof.php
@@ -94,10 +94,38 @@ function listClassOf(){
 
 function addClassOf(){
 	try {
-	    $app = \Slim\Slim::getInstance();
-		$request = $app->request();
-	    $classof_detail = json_decode($request->getBody());
-	    $sql = "INSERT INTO CLASSOF (classof_id, classof_description) VALUES ('$classof_detail->classof_id', '$classof_detail->classof_description')";
+		//check for duplicate first
+		$app = \Slim\Slim::getInstance();
+	    $request = $app->request();
+	    $classof_detail = json_decode($request->getBody());	    
+		$sql = "SELECT COUNT(classof_id) FROM CLASSOF where classof_description = '$classof_detail->classof_description'";
+	} catch(Exception $e) {
+		echo '{"error":{"source":"input","reason":'. $e->getMessage() .'}}';
+		return;
+	}
+    
+	try {
+		$db = new DBManager();
+		$result = $db->getData($sql);
+		$nrow = 0;
+		if ($result){
+			while($row = sqlsrv_fetch_array($result)){
+				$nrow = $row[0];
+			}
+		}
+		$db = null;
+	} catch(PDOException $e) {
+        echo '{"error":{"source":"SQL","reason": SQL'. $e->getMessage() .'}}';
+    }
+
+    if ($nrow > 0) {
+    	//data duplicate
+    	echo '{"error":{"source":"input","reason":"data duplicate"}}';
+		return;
+    }
+
+	try {
+	    $sql = "INSERT INTO CLASSOF (classof_description) VALUES ('$classof_detail->classof_description')";
 	} catch(Exception $e) {
 		echo '{"error":{"source":"input","reason":'. $e->getMessage() .'}}';
 		return;
@@ -125,9 +153,37 @@ function addClassOf(){
 
 function updateClassOf(){
 	try {
-	    $app = \Slim\Slim::getInstance();
-		$request = $app->request();
-	    $classof_detail = json_decode($request->getBody());
+		//check for duplicate first
+		$app = \Slim\Slim::getInstance();
+	    $request = $app->request();
+	    $classof_detail = json_decode($request->getBody());	    
+		$sql = "SELECT COUNT(classof_id) FROM CLASSOF where classof_description = '$classof_detail->classof_description'";
+	} catch(Exception $e) {
+		echo '{"error":{"source":"input","reason":'. $e->getMessage() .'}}';
+		return;
+	}
+    
+	try {
+		$db = new DBManager();
+		$result = $db->getData($sql);
+		$nrow = 0;
+		if ($result){
+			while($row = sqlsrv_fetch_array($result)){
+				$nrow = $row[0];
+			}
+		}
+		$db = null;
+	} catch(PDOException $e) {
+        echo '{"error":{"source":"SQL","reason": SQL'. $e->getMessage() .'}}';
+    }
+
+    if ($nrow > 0) {
+    	//data duplicate
+    	echo '{"error":{"source":"input","reason":"data duplicate"}}';
+		return;
+    }
+
+	try {
 	    $sql = "UPDATE CLASSOF SET classof_description = '$classof_detail->classof_description' where classof_id = '$classof_detail->classof_id'";
 	} catch(Exception $e) {
 		echo '{"error":{"source":"input","reason":'. $e->getMessage() .'}}';
