e66769911c78ac54ec28065e571a60b182835d3a
gie3d <gie68i@gmail.com>
2015-02-21 14:01:57 +0700

    change import student json format (pull classof_id from array of student) and enhance a function to return list of subject by putting a selected flag

--- server/admin_api_import.php
+++ server/admin_api_import.php
@@ -3,7 +3,7 @@
 function importStudents(){
 
 	//sameple data 
-	//$jsonData = '{"data":[{"student_id":47010678,"classofid":"1","name":"name1","lastname":"lastname1","email":"email1@gmail.com","password":"Welcome1","profilepic":"default.jpg","GPA":"3.42","status":"1"},{"student_id":47010677,"classofid":"1","name":"name2","lastname":"lastname2","email":"email2@gmail.com","password":"Welcome1","profilepic":"default.jpg","GPA":"2.59","status":"1"}]}';
+	//$jsonData = '{"data":[{"student_id":47010678,"name":"name1","lastname":"lastname1","email":"email1@gmail.com","password":"Welcome1","profilepic":"default.jpg","GPA":"3.42","status":"1"},{"student_id":47010677,"name":"name2","lastname":"lastname2","email":"email2@gmail.com","password":"Welcome1","profilepic":"default.jpg","GPA":"2.59","status":"1"}],"classof_id": 1}';
 
 	$app = \Slim\Slim::getInstance();
 	$request = $app->request();
@@ -13,9 +13,10 @@ function importStudents(){
 	$db = new DBManager();
 	$db->beginSet();
 	try {
+		$classof_id = $student_arr->classof_id;
 		foreach($student_arr->data as $student){
 			$sql = "merge STUDENT as target
-				using (values ('$student->name', '$student->lastname', '$student->classofid', '$student->email', '$student->password', '$student->profilepic', '$student->GPA', '$student->status'))
+				using (values ('$student->name', '$student->lastname', '$classof_id', '$student->email', '$student->password', '$student->profilepic', '$student->GPA', '$student->status'))
 				    as source (name, lastname, classofid, email, password, profilepic, GPA, student_status)
 				    on target.student_id = '$student->student_id'
 				when matched then
--- server/admin_api_subject.php
+++ server/admin_api_subject.php
@@ -61,10 +61,15 @@ function listSubjectByClassOfAndSemester(){
 	    $request = $app->request();
 	    $classof_id = json_decode($request->getBody())->classof_id;
 	    $semester = json_decode($request->getBody())->semester;
-		$sql = "SELECT a.subject_id, b.name, classof_id, semester, minstudent, maxstudent, credit, dayofweek, timeofday, instructor, isRequired, a.addeddate, a.updatedate 
+		$sql = "SELECT b.subject_id, b.name,
+		CAST(CASE WHEN classof_id is not NULL THEN 1 ELSE 0 END AS bit) AS Selected, 
+		classof_id, 
+		semester, minstudent, maxstudent, 
+		CAST(CASE WHEN credit is not NULL THEN credit ELSE b.defaultpoint END AS float) AS credit
+		, dayofweek, timeofday, instructor, isRequired, a.addeddate, a.updatedate 
 			FROM SUBJECT_CLASSOF a 
-			LEFT JOIN SUBJECT b ON a.subject_id = b.subject_id
-			where classof_id = '$classof_id' and semester = '$semester'";
+			RIGHT JOIN SUBJECT b ON a.subject_id = b.subject_id
+			where (classof_id = '$classof_id' and semester = '$semester') OR (classof_id is NULL and semester is NULL)";
 	} catch(Exception $e) {
 		echo '{"error":{"source":"input","reason":'. $e->getMessage() .'}}';
 		return;
